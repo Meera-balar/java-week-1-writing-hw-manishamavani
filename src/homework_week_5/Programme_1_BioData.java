a71dfcf95f33bd43bff5bd61ac2976c4d3405efb
ManishaMavani <balarmani25@gmail.com>
2022-10-14 22:32:25 +0100

    week_5_homework

---
+++ src/homework_week_5/Programme_1_BioData.java
@@ -0,0 +1,23 @@
+package homework_week_5;
+/*
+ * 1. Write a program that prints your BIO Data.
+ */
+public class Programme_1_BioData {
+    public static void main(String[] argh) {
+        System.out.println("              BIO DATA                ");
+        System.out.println("Name              : Manisha Mavani");
+        System.out.println("Mobile            : 07912 345678");
+        System.out.println("Email             : Manisha20@gmail.com");
+        System.out.println("Gender            : Female");
+        System.out.println("Date of Birth     : 30.07.1995");
+        System.out.println("Marital Status    : Married");
+        System.out.println("Languages Known   : English, Gujarati and Hindi");
+        System.out.println("Qualification     : Software Tester");
+        System.out.println("Experience        : Agile Tester with 5 years of experience");
+        System.out.println("Address           : 168A Abc Street, London AB1 2CD");
+        System.out.println("Place             : London");
+        System.out.println("Date              : 14.10.2022");
+        System.out.println("Signature         : MMavani");
+    }
+
+}
